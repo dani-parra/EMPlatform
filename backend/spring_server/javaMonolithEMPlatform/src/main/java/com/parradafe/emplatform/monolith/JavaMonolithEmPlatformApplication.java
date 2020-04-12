661cd2a5373e5a23b34f488cb0b2ea89c794eb66
parradafe <parrasalamancadaniel@gmail.com>
2020-04-12 13:47:01 -0500

    was added some libraries and the java base project

---
+++ backend/spring_server/javaMonolithEMPlatform/src/main/java/com/parradafe/emplatform/monolith/JavaMonolithEmPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.parradafe.emplatform.monolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMonolithEmPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMonolithEmPlatformApplication.class, args);
+	}
+
+}
---
+++ backend/spring_server/javaMonolithEMPlatform/src/test/java/com/parradafe/emplatform/monolith/JavaMonolithEmPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parradafe.emplatform.monolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMonolithEmPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
