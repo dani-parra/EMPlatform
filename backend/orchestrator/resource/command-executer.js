2cdeb4d52195b44a72ce9ddf2d29aab32bbb0b55
parradafe <parrasalamancadaniel@gmail.com>
2020-04-18 19:45:07 -0500

    was solved some terminal manager issues the command_executer file works successfully

--- backend/orchestrator/resource/command-executer.js
+++ backend/orchestrator/resource/command-executer.js
@@ -1,55 +1,90 @@
 const { exec } = require("child_process");
-const path = require('path');
+const path = require("path");
 
-exports.run = () => {
-    let executionDirectoryPath = path.resolve(process.cwd(), process.env.SERVER_RUNNER_PATH, process.env.SERVER_APP);
-    console.log('[CURRENT_PATH]', executionDirectoryPath)
-    let promisesArray = [];
-    /** Run the server and services sub-servers */
-    /** steps */
+exports.run = async () => {
+  let promisesArray = [];
+  /** set the path for the server */
+  let executionDirectoryPath = path.resolve(
+    process.cwd(),
+    process.env.SERVER_RUNNER_PATH,
+    process.env.SERVER_APP
+  );
 
-    /** Step 1- generate the terminals */
-    promisesArray.push(generateTerminals(1));
-    /** Step 2- run the express and java servers */
-    promisesArray.push(startExpressServer());
+  /** Run the server and services sub-servers */
+  /** steps */
 
-    Promise.all(promisesArray).then(() => {
-        console.log('[SUCCESSFULL PROCESS]');
-    }).catch(err => {
-        console.log('[ERROR]' + err.toString());
-    });
-}
+  /** Step 1- generate the terminals */
+  await generateTerminals(1, executionDirectoryPath);
+  promisesArray.push(startExposeTunnel(executionDirectoryPath));
 
-generateTerminals = terminalNumber => {
-    return new Promise((resolve, reject) => {
-        exec("sudo command_terminals_handler -set " + terminalNumber, { cwd: executionDirectoryPath }, (error, stdout, stderr) => {
-            if (error) {
-                console.log(`error: ${error.message}`);
-                reject(error.message);
-            }
-            if (stderr) {
-                console.log(`stderr: ${stderr}`);
-                reject(stderr);
-            }
-
-            resolve();
-        });
-    })
+  /** Step 2- run the express and java servers */
+  /** set the path for the server */
+  executionDirectoryPath = path.resolve(process.cwd(), "../express_server");
+  await generateTerminals(1, executionDirectoryPath);
+  startExpressServer(executionDirectoryPath);
 };
 
-startExpressServer = () => {
-    let executionDirectoryPath = path.resolve(process.cwd(), process.env.EXPRESS_SERVER_PATH);
-    exec(`PORT=${process.env.EXPRESS_SERVER_PORT} node index`, { cwd: executionDirectoryPath }, (error, stdout, stderr) => {
+generateTerminals = (terminalNumber, executionDirectoryPath) => {
+  return new Promise((resolve, reject) => {
+    exec(
+      "sudo command_terminals_handler -set " + terminalNumber,
+      { cwd: executionDirectoryPath || "" },
+      (error, stdout, stderr) => {
         if (error) {
-            console.log(`error: ${error.message}`);
-            return;
+          console.log(`error: ${error.message}`);
+          reject(error.message);
         }
         if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
+          console.log(`stderr: ${stderr}`);
+          reject(stderr);
+        }
+
+        resolve();
+      }
+    );
+  });
+};
+
+startExposeTunnel = (executionDirectoryPath) => {
+  return new Promise((resolve, reject) => {
+    exec(
+      `command_terminals_handler -run 0 ./ngrok http ${process.env.EXPOSED_PORT}`,
+      { cwd: executionDirectoryPath },
+      (error, stdout, stderr) => {
+        if (error) {
+          console.log(`error: ${error.message}`);
+          reject(error);
+        }
+        if (stderr) {
+          console.log(`stderr: ${stderr}`);
+          reject(stderr);
         }
         console.log(`stdout: ${stdout}`);
-    });
+        resolve();
+      }
+    );
+  });
+};
+
+startExpressServer = executionDirectoryPath => {
+  return new Promise((resolve, reject) => {
+    exec(
+      `command_terminals_handler -run 1 PORT=${process.env.EXPRESS_SERVER_PORT} node index.js`,
+      { cwd: executionDirectoryPath },
+      (error, stdout, stderr) => {
+        if (error) {
+          console.log(`error: ${error.message}`);
+          reject(error);
+        }
+        if (stderr) {
+          console.log(`stderr: ${stderr}`);
+          reject(stderr);
+        }
+        console.log(`stdout: ${stdout}`);
+        resolve();
+      }
+    );
+  });
 };
 
 /* startJavaServer = () => {
